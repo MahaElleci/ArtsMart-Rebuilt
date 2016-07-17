6a998ae9462e3a558493c25c69c3d56d660ab968
HatemMorgan <hatemmorgan17@gmail.com>
2016-07-17 09:06:58 +0200

    finished

---
+++ JavaScript/Products.js
@@ -0,0 +1,11 @@
+$("#list1").on("click", "li", function() {
+    $("#input1").html($(this).text());
+});
+
+$("#list2").on("click", "li", function() {
+    $("#input2").html($(this).text());
+});
+
+$("#list3").on("click", "li", function() {
+    $("#input3").html($(this).text());
+});
